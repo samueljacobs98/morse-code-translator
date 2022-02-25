68d109abc73967bfd58ffde2257c4dd5fef7c108
Samuel Jacobs <samueljacobs@samuels-mbp.home>
2022-02-25 12:18:01 +0000

    linked JavaScript to HTML DOM to make page function properly

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 import {
-    getIndividualMorse,
-    stringToArray,
-    cleanArray,
-    arrayToString,
     translateStringToMorse,
   } from "./functions.js";
 
+// DOM Elements
+const translateToMorseBtn = document.querySelector(".translator__translate-english")
+const englishText = document.querySelector(".translator__english-text");
+const morseText = document.querySelector(".translator__morse-text");
+
+// Functions
+const dealWithTranslate = () => {
+    morseText.value = translateStringToMorse(englishText.value)
+}
+
+// Logic
+translateToMorseBtn.addEventListener("click", dealWithTranslate)
